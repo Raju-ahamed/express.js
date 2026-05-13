eb58ecfcca71b6d6be7f4284a38d3a32762f2433
Raju-ahamed <rajuahamed7320@gmail.com>
2026-05-13 11:44:16 +0600

    3rd repo

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('<div><button>hello <button><div>')
 })
-
+app.get('/data', (req, res) => {
+    res.send("more data here")
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
